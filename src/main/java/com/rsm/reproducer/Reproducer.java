0a176237ceb480860246568fbef2232fe806ace8
Micah Van Deusen <micahjvandeusen@gmail.com>
2022-11-05 16:03:53 -0500

    Add "copy to" to right click context

--- src/main/java/com/rsm/reproducer/Reproducer.java
+++ src/main/java/com/rsm/reproducer/Reproducer.java
@@ -3,10 +3,15 @@
     import burp.api.montoya.BurpExtension;
     import burp.api.montoya.MontoyaApi;
     import burp.api.montoya.http.message.HttpRequestResponse;
+    import burp.api.montoya.http.message.requests.HttpRequest;
     import burp.api.montoya.ui.contextmenu.ContextMenuEvent;
     import burp.api.montoya.ui.contextmenu.ContextMenuItemsProvider;
     import javax.swing.*;
     import static java.util.Collections.emptyList;
+
+    import java.awt.*;
+    import java.awt.datatransfer.Clipboard;
+    import java.awt.datatransfer.StringSelection;
     import java.util.List;
 
     public class Reproducer implements BurpExtension
@@ -39,8 +44,8 @@
             public List<JMenuItem> provideMenuItems(ContextMenuEvent event)
             {
                 if (!event.selectedRequestResponses().isEmpty()) {
-                    JMenuItem menuItem = new JMenuItem("Send to Reproducer");
-                    menuItem.addActionListener(e -> {
+                    JMenuItem sendToReproducerMenuItem = new JMenuItem("Send to Reproducer");
+                    sendToReproducerMenuItem.addActionListener(e -> {
                         tab.selectParentTab();
                         new Thread(() -> {
                             try {
@@ -54,11 +59,35 @@
                             }
                         }).start();
                     });
-                    return List.of(menuItem);
+                    JMenuItem copyPowerShellMenuItem = new JMenuItem("Copy as PowerShell command");
+                    copyPowerShellMenuItem.addActionListener(e -> {
+                        HttpRequest request = event.selectedRequestResponses().get(0).httpRequest();
+                        PowerShellBuilder prb = new PowerShellBuilder(api);
+                        StringSelection stringSelection = new StringSelection(prb.build(request).toString());
+                        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                        clipboard.setContents(stringSelection, null);
+                    });
+                    JMenuItem copyPythonRequestsMenuItem = new JMenuItem("Copy as Python Requests command");
+                    copyPythonRequestsMenuItem.addActionListener(e -> {
+                        HttpRequest request = event.selectedRequestResponses().get(0).httpRequest();
+                        PythonRequestBuilder prb = new PythonRequestBuilder(api);
+                        StringSelection stringSelection = new StringSelection(prb.build(request).toString());
+                        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                        clipboard.setContents(stringSelection, null);
+                    });
+                    JMenuItem copyJavaScriptFetchMenuItem = new JMenuItem("Copy as JavaScript Fetch command");
+                    copyJavaScriptFetchMenuItem.addActionListener(e -> {
+                        HttpRequest request = event.selectedRequestResponses().get(0).httpRequest();
+                        JavaScriptRequestBuilder jrb = new JavaScriptRequestBuilder(api);
+                        StringSelection stringSelection = new StringSelection(jrb.build(request).toString());
+                        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                        clipboard.setContents(stringSelection, null);
+                    });
+                    return List.of(sendToReproducerMenuItem, copyPowerShellMenuItem, copyPythonRequestsMenuItem, copyJavaScriptFetchMenuItem);
                 }
                 else if (event.messageEditorRequestResponse().isPresent()) {
-                    JMenuItem menuItem = new JMenuItem("Send to Reproducer");
-                    menuItem.addActionListener(e -> {
+                    JMenuItem sendToReproducerMenuItem = new JMenuItem("Send to Reproducer");
+                    sendToReproducerMenuItem.addActionListener(e -> {
                         tab.selectParentTab();
                         // This function may make HTTP calls, so run in a thread
                         new Thread(() -> {
@@ -71,7 +100,31 @@
                             }
                         }).start();
                     });
-                    return List.of(menuItem);
+                    JMenuItem copyPowerShellMenuItem = new JMenuItem("Copy as PowerShell command");
+                    copyPowerShellMenuItem.addActionListener(e -> {
+                        HttpRequest request = event.messageEditorRequestResponse().get().getRequestResponse().httpRequest();
+                        PowerShellBuilder prb = new PowerShellBuilder(api);
+                        StringSelection stringSelection = new StringSelection(prb.build(request).toString());
+                        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                        clipboard.setContents(stringSelection, null);
+                    });
+                    JMenuItem copyPythonRequestsMenuItem = new JMenuItem("Copy as Python Requests command");
+                    copyPythonRequestsMenuItem.addActionListener(e -> {
+                        HttpRequest request = event.messageEditorRequestResponse().get().getRequestResponse().httpRequest();
+                        PythonRequestBuilder prb = new PythonRequestBuilder(api);
+                        StringSelection stringSelection = new StringSelection(prb.build(request).toString());
+                        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                        clipboard.setContents(stringSelection, null);
+                    });
+                    JMenuItem copyJavaScriptFetchMenuItem = new JMenuItem("Copy as JavaScript Fetch command");
+                    copyJavaScriptFetchMenuItem.addActionListener(e -> {
+                        HttpRequest request = event.messageEditorRequestResponse().get().getRequestResponse().httpRequest();
+                        JavaScriptRequestBuilder jrb = new JavaScriptRequestBuilder(api);
+                        StringSelection stringSelection = new StringSelection(jrb.build(request).toString());
+                        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                        clipboard.setContents(stringSelection, null);
+                    });
+                    return List.of(sendToReproducerMenuItem, copyPowerShellMenuItem, copyPythonRequestsMenuItem, copyJavaScriptFetchMenuItem);
                 }
                 else
                 {
